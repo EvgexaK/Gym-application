fd9679b56fc20f201300d0e99f4f1f0fb3ca758b
Alex Suslov <suslov@me.com>
2017-11-24 19:37:50 +0300

    title component

--- screens/AccountPage.js
+++ screens/AccountPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import Layout from '../constants/Layout';
-import { backgroundColor } from '../constants/Colors';
-import {titleText} from '../constants/Fonts';
 
-// console.log(backgroundColor);
+import Layout from '../constants/Layout';
+import Title from '../components/Title';
 
 const styles = StyleSheet.create({
-  titleText,
+
 });
 
 class AccountPage extends React.Component {
@@ -18,11 +16,7 @@ class AccountPage extends React.Component {
   render() {
     return (
       <ScrollView>
-        <View
-          style={{ width: '100%', height: 50, backgroundColor}}
-        >
-          <Text style={styles.titleText}>Account</Text>
-        </View>
+        <Title title="Account"/>
         <Text>Hello World</Text>
       </ScrollView>
     );
