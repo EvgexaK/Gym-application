a747da2cc665321a4e28259e81382d4b65b6e8f6
Alex Suslov <suslov@me.com>
2017-11-24 20:32:22 +0300

    label

--- screens/AccountPage.js
+++ screens/AccountPage.js
@@ -4,10 +4,6 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Layout from '../constants/Layout';
 import Title from '../components/Title';
 
-const styles = StyleSheet.create({
-
-});
-
 class AccountPage extends React.Component {
   static navigationOptions = {
     title: 'Account Page',
@@ -17,11 +13,36 @@ class AccountPage extends React.Component {
     return (
       <ScrollView>
         <Title title="Account"/>
+        <View style={styles.column}>
+          <View style={styles.box} >
+            <Text style={styles.textLabel} >left</Text>
+          </View>
+          <View style={styles.box} >
+            <Text style={styles.textLabel} >right</Text>
+          </View>
+        </View>
         <Text>Hello World</Text>
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  column: {
+    flexDirection: 'row',
+    height: 50,
+  },
+  box: {
+    height: 50,
+    width: '50%',
+    backgroundColor: 'black',
+  },
+  textLabel: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
+
 
 export default AccountPage;
 
