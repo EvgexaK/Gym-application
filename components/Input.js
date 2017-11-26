6519d074274c8d2b4e08c24b15c551d462fc4f1a
Alex Suslov <suslov@me.com>
2017-11-26 05:52:02 +0300

    input

--- components/Input.js
+++ components/Input.js
@@ -5,14 +5,18 @@ import { FontAwesome } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   row: {
+    flex: 1,
     width: '100%',
     height: 64,
     backgroundColor: '#262626',
   },
   coner: {
     marginTop: 8,
+    marginRight: 8,
     flexDirection: 'row',
     backgroundColor: '#EEC03C',
+    borderTopRightRadius: 25,
+    borderBottomRightRadius: 25,
   },
   icon: {
     width: 48,
@@ -36,13 +40,19 @@ const styles = StyleSheet.create({
   viewInput:{
     height: 48,
     width: 200,
-    padding: 8,
+    padding: 11,
   },
   textInput: {
     height: 24,
     borderColor: 'gray',
     borderWidth: 1,
     backgroundColor: 'white',
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
   },
 });
 
