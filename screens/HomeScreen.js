2c5ab17baa7e077f8d55a8cf67a123a59c3501f8
EvgexaK <gdanovevgeniy@mail.ru>
2017-12-04 15:13:58 +0200

    HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import {
   Image,
   Platform,
@@ -20,9 +18,6 @@ import ImageExercise from '../components/ImageExercise';
 import ImageExercise1 from '../components/ImageExercise1';
 import InputTest from '../components/InputTest';
 
-
-const remote = 'https://s15.postimg.org/tw2qkvmcb/400px.png';
-
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
@@ -41,16 +36,9 @@ class HomeScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <ScrollView vertical>
-        <ImageExercise1
-          handlePress={() =>
-            this.props.navigation.navigate('SingleExercise', {
-              name: 'Benchpress',
-            })}
-          imageSource={require('../assets/images/start.png')}
-          color={'#333333'}
-        />
-      </ScrollView>
+        <Image
+          source={require('../assets/images/ImageThumbnail.png')}
+        />    
     );
   }
 }
@@ -63,4 +51,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default connect(s => s.member) (HomeScreen);
+export default HomeScreen;
